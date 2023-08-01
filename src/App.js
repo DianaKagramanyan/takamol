d625cc382e0fe331470275561c69a450dced713b
dianakagramanyan <diana.kagramanyan1@gmail.com>
2023-08-01 14:06:37 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import Header from "./public/homepage/components/header/Header";
-import Home from "./public/homepage/components/home/Home";
 import Footer from "./public/homepage/components/footer/Footer";
+import {Route, Routes} from "react-router-dom";
+import About from "./public/homepage/pages/About";
+import Services from "./public/homepage/pages/Services";
+import Faq from "./public/homepage/pages/Faq";
+import Home from "./public/homepage/pages/Home";
+import MainContent from "./public/homepage/components/home/MainContent";
 
 
 function App() {
   return (
     <div>
       <Header/>
-      <Home/>
+      <Routes>
+        <Route path="Main" element={<Home/>}/>
+        <Route path="About Platform" element={<About/>}/>
+        <Route path="Our Services" element={<Services/>}/>
+        <Route path="Help" element={<Faq/>}/>
+      </Routes>
+      <MainContent/>
       <Footer/>
     </div>
   );
--- src/public/homepage/pages/About.js
+++ src/public/homepage/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      About the platform
+      About the platform....
     </div>
   );
 };
