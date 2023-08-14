84f96f70cfa0bd69731cedf5588a2e98a799a6c1
dianakagramanyan <diana.kagramanyan1@gmail.com>
2023-08-14 09:39:07 -0400

    added data

--- src/public/help/cards/ShowCards.js
+++ src/public/help/cards/ShowCards.js
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 import list from './data';
 import Card from './Card';
+import {Pagination, Stack} from "@mui/material";
 
 const ShowCards = () => {
+  const [page, setPage] = useState(1);
+
+  const handleChange = (e, p) => {
+    console.log(e, p);
+    setPage(p)
+  }
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const recordsPerPage = 8;
+  // const lastIndex = currentPage * recordsPerPage;
+  // const firstIndex = lastIndex - recordsPerPage;
+  // const records = list.slice(firstIndex, lastIndex);
+  // const npage = Math.ceil(list.length / recordsPerPage);
+  // const numbers = [...Array(npage + 1).keys()].slice(1);
+  //
+  // const prePage = () => {
+  //   if (currentPage !== 1) {
+  //     setCurrentPage(currentPage - 1);
+  //   }
+  // }
+  // const changeCPage = (id) => {
+  //   setCurrentPage(id);
+  // }
+  // const nextPage = () => {
+  //   if (currentPage !== npage) {
+  //     setCurrentPage(currentPage + 1);
+  //   }
+  // }
+
   return (
-    <section>
-      {
-        list.map((item) =>
-          (<Card key={item.id} item={item}/>
-          ))}
-    </section>
+    <div className="section-1">
+      <section>
+        {
+          list.map((item) =>
+            (<Card key={item.id} item={item}/>
+            ))}
+      </section>
+      <Stack className="pagination" spacing={2}>
+        <Pagination count={8} onClick={handleChange}/>
+      </Stack>
+
+      {/*<nav>*/}
+      {/*  <ul>*/}
+      {/*    <li>*/}
+      {/*      <a href="#" onClick={prePage}>Prev</a>*/}
+      {/*    </li>*/}
+      {/*    {*/}
+      {/*      numbers.map((n, i) => (*/}
+      {/*        <li key={i}>*/}
+      {/*          <a href="#" onClick={() => changeCPage(n)}>{n}</a>*/}
+      {/*        </li>*/}
+      {/*      ))*/}
+      {/*    }*/}
+      {/*    <li>*/}
+      {/*      <a href="#" onClick={nextPage}>Next</a>*/}
+      {/*    </li>*/}
+      {/*  </ul>*/}
+      {/*</nav>*/}
+    </div>
   );
 };
 
--- src/public/help/cards/data.js
+++ src/public/help/cards/data.js
@@ -39,6 +39,126 @@ const list = [
     id: 10,
     title: "Request to cancel a license for private centers for people with special needs",
   },
+  {
+    id: 11,
+    title: "Request to cancel a license for private childcare centers",
+  },
+  {
+    id: 12,
+    title: "Request to cancel a license for the National Elderly Center",
+  },
+  {
+    id: 13,
+    title: "Request to cancel work suspension in private family counseling centers",
+  },
+  {
+    id: 14,
+    title: "Request to cancel the suspension of work in centers for people with civil disabilities",
+  },
+  {
+    id: 15,
+    title: "Request to cancel work suspension in private childcare centers",
+  },
+  {
+    id: 16,
+    title: "Request to cancel the suspension of work in the Elderly National Center",
+  },
+  {
+    id: 17,
+    title: "Hosting request service with a friendly family and my role model program",
+  },
+  {
+    id: 18,
+    title: "Service of requesting renewal of a final license for private family counseling centers (corporate capacity)",
+  },
+  {
+    id: 19,
+    title: "Request to renew a final license for private family counseling centers (natural capacity)",
+  },
+  {
+    id: 20,
+    title: "Request to renew a final license for private children's hospitality centers (corporate capacity)",
+  },
+  {
+    id: 21,
+    title: "Request service to renew a final license for private childcare centers (natural capacity)",
+  },
+  {
+    id: 22,
+    title: "Request service to renew a final license for the National Elderly Center (legal capacity)",
+  },
+  {
+    id: 23,
+    title: "Request service to renew a final license for the National Elderly Center (natural capacity)",
+  },
+  {
+    id: 24,
+    title: "Disability classification request service for undergraduate students and the subsidy category",
+  },
+  {
+    id: 25,
+    title: "Service of requesting suspension of work in private family counseling centers",
+  },
+  {
+    id: 26,
+    title: "Service of requesting suspension of work in centers for people with civil disabilities",
+  },
+  {
+    id: 27,
+    title: "Service of requesting suspension of work in private childcare centers",
+  },
+  {
+    id: 28,
+    title: "Service of requesting suspension of work in the Elderly National Center",
+  },
+  {
+    id: 29,
+    title: "Disability assessment request service",
+  },
+  {
+    id: 30,
+    title: "Request to extend the suspension of work in centers for people with civil disabilities",
+  },
+  {
+    id: 31,
+    title: "Request to extend work suspension in private childcare centers",
+  },
+  {
+    id: 32,
+    title: "Request to extend the suspension of work at the Elderly National Center",
+  },
+  {
+    id: 33,
+    title: "Service of requesting a letter of recommendation for orphans",
+  },
+  {
+    id: 34,
+    title: "User Guide Issuance of initial approval service for centers for people with civil disabilities (natural capacity)",
+  },
+  {
+    id: 35,
+    title: "User Guide Issuance of initial approval service for a civil family counseling center (natural capacity)",
+  },
+  {
+    id: 36,
+    title: "User Guide Issuance of Initial Approval Service for the Elderly Family Center (Natural Capacity)",
+  },
+  {
+    id: 37,
+    title: "User's Guide Ownership transfer service for private children's hospitality centers",
+  },
+  {
+    id: 38,
+    title: "User guide for issuing a license for private childcare centers",
+  },
+  {
+    id: 39,
+    title: "User guide for the service of issuing a license for private family counseling centers",
+  },
+  {
+    id: 40,
+    title: "User guide for the service of issuing a license for the National Elderly Center",
+  },
 ];
 
 export default list;
