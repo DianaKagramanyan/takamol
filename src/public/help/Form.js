3fec84679a8f5b662af43483fd23f81157d3b02f
dianakagramanyan <diana.kagramanyan1@gmail.com>
2023-08-14 13:24:19 -0400

    added styling

--- src/public/help/Form.js
+++ src/public/help/Form.js
@@ -5,6 +5,7 @@ import YouTubeIcon from "@mui/icons-material/YouTube";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import OutlinedInput from '@mui/material/OutlinedInput';
 import Button from "@mui/material/Button";
+import PlaceIcon from '@mui/icons-material/Place';
 
 const Form = () => {
   return (
@@ -27,22 +28,27 @@ const Form = () => {
 
 
         <div className="location">
-          <div>Our Location</div>
-          <div>Building A6, Granada Office Oasis</div>
-          <div>Eastern Ring Road, Al Shuhada District</div>
-          <div>Riyadh, 13241</div>
-          <div>Kingdom of Saudi Arabia</div>
-          <div>You can also communicate through social networking pages</div>
-          <div>
-            <Link to="https://www.instagram.com/hrsd_sa/?igshid=YmMyMTA2M2Y%3D" target="_blank">
-              <InstagramIcon className="icon-three" sx={{color: "#3C8084", fontSize: 30}}/>
-            </Link>
-            <Link to="https://www.youtube.com/c/HRSDsa" target="_blank">
-              <YouTubeIcon className="icon-three" sx={{color: "#3C8084", fontSize: 30}}/>
-            </Link>
-            <Link to="https://twitter.com/hrsd_sa?s=21&t=oVEYi49T6hc944BpCiHpVQ" target="_blank">
-              <TwitterIcon className="icon-three" sx={{color: "#3C8084", fontSize: 30}}/>
-            </Link>
+          <div className="first-loc">
+            <div className="our-location">Our Location <PlaceIcon sx={{color: "rgb(250, 180, 20)"}}/></div>
+            <div className="det">Building A6, Granada Office Oasis</div>
+            <div className="det">Eastern Ring Road, Al Shuhada District</div>
+            <div className="det">Riyadh, 13241</div>
+            <div className="det">Kingdom of Saudi Arabia</div>
+          </div>
+
+          <div className="second-loc">
+            <div className="det">You can also communicate through social networking pages</div>
+            <div>
+              <Link to="https://www.instagram.com/hrsd_sa/?igshid=YmMyMTA2M2Y%3D" target="_blank">
+                <InstagramIcon className="icon-three" sx={{color: "#3C8084", fontSize: 30}}/>
+              </Link>
+              <Link to="https://www.youtube.com/c/HRSDsa" target="_blank">
+                <YouTubeIcon className="icon-three" sx={{color: "#3C8084", fontSize: 30}}/>
+              </Link>
+              <Link to="https://twitter.com/hrsd_sa?s=21&t=oVEYi49T6hc944BpCiHpVQ" target="_blank">
+                <TwitterIcon className="icon-three" sx={{color: "#3C8084", fontSize: 30}}/>
+              </Link>
+            </div>
           </div>
         </div>
 
