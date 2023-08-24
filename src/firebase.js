0ac61d4bd898f442a4b06bd1518dc4ca2470feb5
dianakagramanyan <diana.kagramanyan1@gmail.com>
2023-08-24 15:09:42 -0400

    corrected styles in login and create

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCmwXfLbzhjjSyDZl2il71j53g1bXJXXfg",
-  authDomain: "fir-auth-39f58.firebaseapp.com",
-  projectId: "fir-auth-39f58",
-  storageBucket: "fir-auth-39f58.appspot.com",
-  messagingSenderId: "29039366211",
-  appId: "1:29039366211:web:dd7683bd2ad42ac11d44f6"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+//
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCmwXfLbzhjjSyDZl2il71j53g1bXJXXfg",
+//   authDomain: "fir-auth-39f58.firebaseapp.com",
+//   projectId: "fir-auth-39f58",
+//   storageBucket: "fir-auth-39f58.appspot.com",
+//   messagingSenderId: "29039366211",
+//   appId: "1:29039366211:web:dd7683bd2ad42ac11d44f6"
+// };
+//
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+//
