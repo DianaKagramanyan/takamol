fe16aec44239d4a71773b0f31518815b98af35a1
dianakagramanyan <diana.kagramanyan1@gmail.com>
2023-08-28 15:19:33 -0400

    sidebar done

--- src/public/dashboard/ClippedBar.js
+++ src/public/dashboard/ClippedBar.js
@@ -1,19 +1,20 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
 import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import HeaderDashboard from "./HeaderDashboard";
+import DesktopWindowsOutlinedIcon from '@mui/icons-material/DesktopWindowsOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import IconButton from "@mui/material/IconButton";
+import {logo} from "../../img";
+
 
 const drawerWidth = 240;
 
@@ -30,44 +31,72 @@ export default function ClippedBar() {
           [`& .MuiDrawer-paper`]: {width: drawerWidth, boxSizing: 'border-box'},
         }}
       >
-        <Toolbar/>
-        <Box sx={{overflow: 'auto'}}>
-          <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
-                  </ListItemIcon>
-                  <ListItemText primary={text}/>
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-          <Divider/>
-          <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
-                  </ListItemIcon>
-                  <ListItemText primary={text}/>
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
+        <Box className="drawer">
+          <Box>
+            <div>
+              <Toolbar/>
+            </div>
+            <Box>
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-haspopup="true"
+              >
+                <AccountCircle sx={{color: 'gray', fontSize: "50px", marginRight: '10px'}}/>
+                <Typography sx={{color: 'black'}}>Diana Kagramanyan</Typography>
+              </IconButton>
+            </Box>
+
+            <Box  sx={{overflow: 'auto'}}>
+              <List className="list-container">
+                {['Data panel', 'Services', 'Positions', 'Requests', 'User guides'].map((text, index) => (
+                  <ListItem key={text} disablePadding>
+                    <ListItemButton>
+                      <ListItemIcon>
+                        {index % 2 === 0 ? <DesktopWindowsOutlinedIcon sx={{color: '#3C8084'}}/> :
+                          <HomeOutlinedIcon sx={{color: '#3C8084'}}/>}
+                      </ListItemIcon>
+                      <ListItemText primary={text}/>
+                    </ListItemButton>
+                  </ListItem>
+                ))}
+              </List>
+            </Box>
+          </Box>
+
+
+          <Box className="list-container">
+            <Box>
+              <IconButton
+                size="large"
+                edge="start"
+                color=""
+                aria-label="open drawer"
+                sx={{mr: 2}}
+              >
+                <img src={logo} alt="logoImage"/>
+              </IconButton>
+            </Box>
+            <Box>
+              <Typography sx={{fontSize: "12px"}}>All rights reserved to Rehabilitation and Social
+                Guidance</Typography>
+              <Typography sx={{fontSize: "12px"}}>Platform | Qualification 2023</Typography>
+            </Box>
+          </Box>
         </Box>
+
+
       </Drawer>
-      <Box component="main" sx={{flexGrow: 1, p: 3}}>
-        <Toolbar/>
-        <Typography paragraph>
-sdsadsa
-        </Typography>
-        <Typography paragraph>
-ssfsf
-        </Typography>
-      </Box>
+      {/*<Box component="main" sx={{flexGrow: 1, p: 3}}>*/}
+      {/*  <Toolbar/>*/}
+      {/*  <Typography paragraph>*/}
+      {/*    sdsadsa*/}
+      {/*  </Typography>*/}
+      {/*  <Typography paragraph>*/}
+      {/*    ssfsf*/}
+      {/*  </Typography>*/}
+      {/*</Box>*/}
     </Box>
   );
 }
